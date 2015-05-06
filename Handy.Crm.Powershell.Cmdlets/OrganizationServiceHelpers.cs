e628c99713c13f09521197860559ee3a697fba83
yanety <traaance@gmail.com>
2015-05-06 16:01:50 +0300

    Added ability to retrieve more than 5k records via Get-CRMEntity cmdlet.

---
+++ Handy.Crm.Powershell.Cmdlets/OrganizationServiceHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+
+namespace Handy.Crm.Powershell.Cmdlets
+{
+	static class OrganizationServiceHelpers
+	{
+		public static List<Entity> RetrieveMultipleAll(this OrganizationService organizationService, FetchExpression query)
+		{
+			int fetchCount = 5000;
+			int pageNumber = 1;
+			string pagingCookie = null;
+
+			EntityCollection pageResult;
+			List<Entity> result = new List<Entity>();
+
+			do
+			{
+				query.MakePaged(fetchCount, pageNumber, pagingCookie);
+
+				pageResult = organizationService.RetrieveMultiple(query);
+				result.AddRange(pageResult.Entities.ToList());
+
+				pageNumber++;
+				pagingCookie = pageResult.PagingCookie;
+
+			} while (pageResult.MoreRecords);
+
+			return result;
+		}
+	}
+}
