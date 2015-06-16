730ebbbac647e07949c2cad5a0fbd66be05a0a44
yanety <traaance@gmail.com>
2015-06-16 10:10:48 +0300

    Added parameter whether to publish workflows during solution importing or no.

--- Handy.Crm.Powershell.Cmdlets/ImportCrmSolutionCommand.cs
+++ Handy.Crm.Powershell.Cmdlets/ImportCrmSolutionCommand.cs
@@ -5,41 +5,46 @@ using Microsoft.Crm.Sdk.Messages;
 
 namespace Handy.Crm.Powershell.Cmdlets
 {
-  [Cmdlet(VerbsData.Import, "CRMSolution")]
-  public class ImportCrmSolutionCommand : CrmCmdletBase
-  {
-    [Parameter(
-      Mandatory = true),
-    ValidateNotNullOrEmpty]
-    public string Path { get; set; }
+	[Cmdlet(VerbsData.Import, "CRMSolution")]
+	public class ImportCrmSolutionCommand : CrmCmdletBase
+	{
+		[Parameter(
+			Mandatory = true),
+		ValidateNotNullOrEmpty]
+		public string Path { get; set; }
 
-    [Parameter(
-      Mandatory = false)]
-    public SwitchParameter OverwriteCustomizations { get; set; }
+		[Parameter(
+			Mandatory = false)]
+		public SwitchParameter OverwriteCustomizations { get; set; }
 
-    [Parameter(
-      Mandatory = false)]
-    public SwitchParameter PassThru { get; set; }
+		[Parameter(
+			Mandatory = false)]
+		public SwitchParameter PublishWorkflows { get; set; }
 
-    protected override void ProcessRecord()
-    {
-      base.ProcessRecord();
+		[Parameter(
+			Mandatory = false)]
+		public SwitchParameter PassThru { get; set; }
 
-      byte[] fileBytes = File.ReadAllBytes(Path);
+		protected override void ProcessRecord()
+		{
+			base.ProcessRecord();
 
-      ImportSolutionRequest impSolutionRequest = new ImportSolutionRequest()
-      {
-        CustomizationFile = fileBytes,
-        OverwriteUnmanagedCustomizations = (bool)OverwriteCustomizations,
-        ImportJobId = Guid.NewGuid()
-      };
+			byte[] fileBytes = File.ReadAllBytes(Path);
 
-      WriteVerbose(string.Format("Start importing solution from {0}", Path));
-      organizationService.Execute(impSolutionRequest);
-      WriteVerbose("Finished importing solution");
+			ImportSolutionRequest impSolutionRequest = new ImportSolutionRequest()
+			{
+				CustomizationFile = fileBytes,
+				ImportJobId = Guid.NewGuid(),
+				OverwriteUnmanagedCustomizations = OverwriteCustomizations,
+				PublishWorkflows = PublishWorkflows
+			};
 
-      if (PassThru)
-        WriteObject(impSolutionRequest.ImportJobId);
-    }
-  }
+			WriteVerbose(string.Format("Start importing solution from {0}", Path));
+			organizationService.Execute(impSolutionRequest);
+			WriteVerbose("Finished importing solution");
+
+			if (PassThru)
+				WriteObject(impSolutionRequest.ImportJobId);
+		}
+	}
 }
