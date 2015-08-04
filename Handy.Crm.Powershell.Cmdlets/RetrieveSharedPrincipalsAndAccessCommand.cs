ee5604df1d1d59b78463740de2cfa5f5446d1012
sergeytunnik <traaance@gmail.com>
2015-08-04 18:06:45 +0300

    Implementerd cmdlets  for RevokeAccessRequest and RetrieveSharedPrincipalsAndAccessRequest.

---
+++ Handy.Crm.Powershell.Cmdlets/RetrieveSharedPrincipalsAndAccessCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+
+namespace Handy.Crm.Powershell.Cmdlets
+{
+	[Cmdlet(VerbsCommon.Get, "CRMSharedPrincipalsAndAccess")]
+	public class RetrieveSharedPrincipalsAndAccessCommand : CrmCmdletBase
+	{
+		[Parameter(Mandatory = true)]
+		public EntityReference Target { get; set; }
+
+		protected override void ProcessRecord()
+		{
+			base.ProcessRecord();
+
+			RetrieveSharedPrincipalsAndAccessRequest request = new RetrieveSharedPrincipalsAndAccessRequest()
+			{
+				Target = Target
+			};
+
+			WriteVerbose("Executing RetrieveSharedPrincipalsAndAccessRequest");
+			RetrieveSharedPrincipalsAndAccessResponse response = (RetrieveSharedPrincipalsAndAccessResponse)organizationService.Execute(request);
+
+			WriteObject(response);
+		}
+	}
+}
---
+++ Handy.Crm.Powershell.Cmdlets/RevokeAccessCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+
+namespace Handy.Crm.Powershell.Cmdlets
+{
+	[Cmdlet(VerbsSecurity.Revoke, "CRMAccess")]
+	public class RevokeAccessCommand : CrmCmdletBase
+	{
+		[Parameter(Mandatory = true)]
+		public EntityReference Revokee { get; set; }
+
+		[Parameter(Mandatory = true)]
+		public EntityReference Target { get; set; }		
+
+		protected override void ProcessRecord()
+		{
+			base.ProcessRecord();
+
+			RevokeAccessRequest request = new RevokeAccessRequest()
+			{
+				Revokee = Revokee,
+				Target = Target
+			};
+
+			WriteVerbose("Executing RevokeAccessResponse");
+			RevokeAccessResponse response = (RevokeAccessResponse)organizationService.Execute(request);
+
+			WriteObject(response);
+		}
+	}
+}
