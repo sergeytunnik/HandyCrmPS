f552633bcfd87ad243955a7d4f126abac100c59b
sergeytunnik <traaance@gmail.com>
2015-10-20 13:59:04 +0300

    Ability to use global variable instead of specifying Connection parameter for each cmdlet.

--- Handy.Crm.Powershell.Cmdlets/CrmCmdletBase.cs
+++ Handy.Crm.Powershell.Cmdlets/CrmCmdletBase.cs
@@ -4,30 +4,48 @@ using Microsoft.Xrm.Client.Services;
 
 namespace Handy.Crm.Powershell.Cmdlets
 {
-  public class CrmCmdletBase : PSCmdlet
-  {
-    protected OrganizationService organizationService { get; set; }
+	public class CrmCmdletBase : PSCmdlet
+	{
+		private const string GlobalConnectionVariableName = "HandyCrmDefaultConnection";
+		protected OrganizationService organizationService { get; set; }
 
-    [Parameter(
-      Mandatory = true,
+		[Parameter(
+			Mandatory = false,
 			Position = 0)]
-    [ValidateNotNull]
-    public CrmConnection Connection { get; set; }
+		[ValidateNotNull]
+		public CrmConnection Connection { get; set; }
 
-    protected override void BeginProcessing()
-    {
-      base.BeginProcessing();
+		protected override void BeginProcessing()
+		{
+			base.BeginProcessing();
 
-      WriteVerbose("Creating OrganizationService");
-			organizationService = new OrganizationService(Connection);
-    }
+			WriteVerbose("Creating OrganizationService");
+			organizationService = new OrganizationService(Connection ?? GetConnectionFromPSVariable(GlobalConnectionVariableName));
+		}
 
-    protected override void EndProcessing()
-    {
-      WriteVerbose("Disposing OrganizationService");
-      organizationService.Dispose();
+		protected override void EndProcessing()
+		{
+			WriteVerbose("Disposing OrganizationService");
+			organizationService.Dispose();
 
 			base.EndProcessing();
-    }
-  }
+		}
+
+		private CrmConnection GetConnectionFromPSVariable(string VariableName)
+		{
+			WriteVerbose(string.Format("Getting connection from global variable '{0}'.", VariableName));
+
+			var psobj = SessionState.PSVariable.GetValue(string.Format("global:{0}", VariableName), null);
+
+			if (psobj == null)
+				throw new PSArgumentNullException(VariableName, string.Format("Couldn't find global variable '{0}'.", VariableName));
+
+			var connectionobj = psobj.UnwrapPSObject();
+
+			if (!(connectionobj is Microsoft.Xrm.Client.CrmConnection))
+				throw new PSArgumentException(string.Format("Variable '{0}' isn't an instance of Microsoft.Xrm.Client.CrmConnection.", VariableName), VariableName);
+
+			return (CrmConnection)connectionobj;
+		}
+	}
 }
