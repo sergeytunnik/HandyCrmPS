92252ff33ab9d7f05c081d70daa974759eb57173
sergeytunnik <traaance@gmail.com>
2016-11-04 17:33:41 +0200

    Added ability to update entity by id witout passing whole Entity object.

--- Handy.Crm.Powershell.Cmdlets/UpdateCrmEntityCommand.cs
+++ Handy.Crm.Powershell.Cmdlets/UpdateCrmEntityCommand.cs
@@ -1,32 +1,76 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.Collections;
+using System.Management.Automation;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 
 namespace Handy.Crm.Powershell.Cmdlets
 {
-	[Cmdlet(VerbsData.Update, "CRMEntity")]
-	public class UpdateCrmEntityCommand : CrmExecuteMultipleCmdletBase
-	{
-		[Parameter(
-			Mandatory = true,
-			ValueFromPipeline = true)]
-		[ValidateNotNullOrEmpty]
-		public Entity[] Entity { get; set; }
+  [Cmdlet(VerbsData.Update, "CRMEntity")]
+  public class UpdateCrmEntityCommand : CrmExecuteMultipleCmdletBase
+  {
+    private const string ParameterSetNameEntity = "Entity";
+    private const string ParameterSetNameHashtable = "Hashtable";
 
-		protected override void ProcessRecord()
-		{
-			base.ProcessRecord();
+    [Parameter(
+      Mandatory = true,
+      ValueFromPipeline = true,
+      ParameterSetName = ParameterSetNameEntity)]
+    [ValidateNotNullOrEmpty]
+    public Entity[] Entity { get; set; }
 
-			WriteVerbose("Entering in ProcessRecord");
+    [Parameter(
+      Mandatory = true,
+      ParameterSetName = ParameterSetNameHashtable)]
+    [ValidateNotNullOrEmpty]
+    public string EntityName { get; set; }
 
-			foreach (Entity e in Entity)
-			{
-				WriteVerbose(string.Format("Adding {0} ({1}) into ExecuteMultipleRequest", e.LogicalName, e.Id));
+    [Parameter(
+      Mandatory = true,
+      ParameterSetName = ParameterSetNameHashtable)]
+    [ValidateNotNull]
+    public Guid Id { get; set; }
 
-				e.UnwrapAttributes();
-				UpdateRequest updateRequest = new UpdateRequest { Target = e };
-				_executeMultipleRequest.Requests.Add(updateRequest);
-			}
-		}
-	}
+    [Parameter(
+      Mandatory = true,
+      ParameterSetName = ParameterSetNameHashtable)]
+    [ValidateNotNull]
+    public Hashtable Attributes { get; set; }
+
+    protected override void ProcessRecord()
+    {
+      base.ProcessRecord();
+
+      WriteVerbose("Entering in ProcessRecord");
+
+      switch (ParameterSetName)
+      {
+        case ParameterSetNameEntity:
+          foreach (var e in Entity)
+          {
+            WriteVerbose($"Adding {e.LogicalName} ({e.Id}) into ExecuteMultipleRequest");
+
+            e.UnwrapAttributes();
+
+            _executeMultipleRequest.Requests.Add(new UpdateRequest { Target = e });
+          }
+          break;
+
+        case ParameterSetNameHashtable:
+          WriteVerbose($"Adding {EntityName} ({Id}) into ExecuteMultipleRequest");
+
+          var entityToUpdate = new Entity(EntityName, Id);
+
+          foreach (string key in Attributes.Keys)
+          {
+            entityToUpdate[key] = Attributes[key].UnwrapPSObject();
+          }
+
+          _executeMultipleRequest.Requests.Add(new UpdateRequest {Target = entityToUpdate});
+          break;
+      }
+
+
+    }
+  }
 }
